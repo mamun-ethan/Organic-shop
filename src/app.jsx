d46779047bd543c1d19897451d57dd4d7655eca8
mamun ethan <mamunsikder73375@gmail.com>
2024-12-10 13:10:05 +0600

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-600 text-4xl text-center  capitalize font-bold font-lora">
+        Hi im app compoents
+      </h1>
+      <p>klasdhfpioasfpoas</p>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      lora: ["Lora", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
