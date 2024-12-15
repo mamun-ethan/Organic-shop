e4fb6c84e45c00a170c7c6eadc0b985b1f15bec9
mamun ethan <mamunsikder73375@gmail.com>
2024-12-15 15:38:54 +0600

    brand section creation complete

---
+++ client-side/src/components/brands/brand.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import BrandOne from "../../assets/image/brands/brand-1.png";
+import BrandTwo from "../../assets/image/brands/brand-2.png";
+import BrandThree from "../../assets/image/brands/brand-3.png";
+import BrandFour from "../../assets/image/brands/brand-4.png";
+import BrandFive from "../../assets/image/brands/brand-5.png";
+import BrandSix from "../../assets/image/brands/brand-6.png";
+
+export default function brand() {
+  return (
+    <div className="my-5">
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+          <div>
+            <img src={BrandOne} alt="" />
+          </div>
+          <div>
+            <img src={BrandTwo} alt="" />
+          </div>
+          <div>
+            <img src={BrandThree} alt="" />
+          </div>
+          <div>
+            <img src={BrandFour} alt="" />
+          </div>
+          <div>
+            <img src={BrandFive} alt="" />
+          </div>
+          <div>
+            <img src={BrandSix} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client-side/src/components/pages/home/home.jsx
+++ client-side/src/components/pages/home/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../../layout/layout";
 import Featured from "../../featured/featured";
+import Brand from "../../brands/brand";
 export default function home() {
   return (
     <>
       <Layout>
         <Featured />
+        <Brand />
       </Layout>
     </>
   );
