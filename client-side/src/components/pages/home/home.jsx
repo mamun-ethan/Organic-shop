387fc6e0e858ae6da5f437d603d50900db2f767f
mamun ethan <mamunsikder73375@gmail.com>
2024-12-15 12:11:26 +0600

    featured added

--- client-side/src/components/pages/home/home.jsx
+++ client-side/src/components/pages/home/home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import Layout from "../../layout/layout";
+import Featured from "../../featured/featured";
 export default function home() {
-  return <Layout></Layout>;
+  return (
+    <>
+      <Layout>
+        <Featured />
+      </Layout>
+    </>
+  );
 }
